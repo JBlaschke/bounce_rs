8cf0384b1efdc64f42ba39cf4377769e25086a7c
Johannes Blaschke <jpblaschke@lbl.gov>
2024-10-26 15:36:54 -0700

    gerneralize code

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,30 @@ use pingora::prelude::*;
 use std::sync::Arc;
 
 
-pub struct LB(Arc<LoadBalancer<RoundRobin>>);
+pub struct LB {
+    lb:   Arc<LoadBalancer<RoundRobin>>,
+    host: String
+}
 
 
 #[async_trait]
 impl ProxyHttp for LB {
 
-    /// For this small example, we don't need context storage
+    // We don't need context storage for now
     type CTX = ();
-    fn new_ctx(&self) -> () {
-        ()
-    }
+    fn new_ctx(&self) -> () {()}
 
     async fn upstream_peer(
         &self, _session: &mut Session, _ctx: &mut ()
     ) -> Result<Box<HttpPeer>> {
-        let upstream = self.0
+        let upstream = self.lb
             .select(b"", 256) // hash doesn't matter for round robin
             .unwrap();
 
         println!("upstream peer is: {upstream:?}");
 
         // Set SNI to one.one.one.one
-        let peer = Box::new(
-            HttpPeer::new(upstream, true, "pswww.slac.stanford.edu".to_string())
-        );
+        let peer = Box::new(HttpPeer::new(upstream, true, self.host.clone()));
         Ok(peer)
     }
 
@@ -35,23 +34,27 @@ impl ProxyHttp for LB {
         &self, _session: &mut Session, upstream_request: &mut RequestHeader,
         _ctx: &mut Self::CTX,
     ) -> Result<()> {
-        upstream_request.insert_header("Host", "pswww.slac.stanford.edu").unwrap();
+        upstream_request.insert_header("Host", self.host.clone()).unwrap();
         Ok(())
     }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let remote_host: String = "pswww.slac.stanford.edu".to_string();
+    let upstream_addr = format!("{}:{}", remote_host, 443);
+    println!("Redirecting to: {}", upstream_addr);
 
     let mut my_server = Server::new(None).unwrap();
     my_server.bootstrap();
 
     let upstreams =
-        LoadBalancer::try_from_iter(["pswww.slac.stanford.edu:443",]).unwrap();
+        LoadBalancer::try_from_iter([upstream_addr,]).unwrap();
 
     let mut lb = http_proxy_service(
-        &my_server.configuration, LB(Arc::new(upstreams))
+        &my_server.configuration, LB{
+            lb: Arc::new(upstreams), host: remote_host
+        }
     );
     lb.add_tcp("0.0.0.0:6188");
 
